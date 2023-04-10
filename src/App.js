07cdf9ea93749a876240204603c73a91374f512f
Gianni Narracci <gnarracci@gmail.com>
2023-04-10 03:29:19 -0400

    Delete Citas Config and Message

--- src/App.js
+++ src/App.js
@@ -12,6 +12,15 @@ function App() {
     guardarCitas([...citas, cita]);
   }
 
+  // Funcion que elimina una cita por su id
+  const eliminarCita = id => {
+    const nuevasCitas = citas.filter(cita => cita.id !== id );
+    guardarCitas(nuevasCitas);
+  }
+
+  // Mensaje Condicional
+  const titulo = citas.length === 0 ? "No hay Citas" : "Administra tus Citas";
+
   return (
     <Fragment>
       <h1>Administrador de Pacientes</h1>
@@ -23,11 +32,12 @@ function App() {
             />
           </div>
           <div className="one-half column">
-            <h2>Administra tus Citas</h2>
+            <h2>{titulo}</h2>
             {citas.map(cita => (
               <Cita
                 key={cita.id}
                 cita={cita}
+                eliminarCita={eliminarCita}
               />
             ))}
           </div>
